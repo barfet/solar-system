a918484d2a0d01700408e315070a32ff11e73fd9
RomanBorysenok <Roman_Borysenok@epam.com>
2017-08-22 17:42:58 +0300

    testing

--- apps/home/src/app/app.component.ts
+++ apps/home/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { State } from './state/reducer';
+
+import { Observable } from 'rxjs/Observable';
+import { SimplePlanetInfo } from './types'
+import { PlanetsFetchAction } from './state/planets/actions';
 
 import '../styles/app.scss';
 
@@ -7,10 +13,21 @@ import '../styles/app.scss';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   url = 'https://github.com/preboot/angular2-webpack';
   title: string;
+  private planets: Observable<SimplePlanetInfo[]>;
 
-  constructor() {
+  constructor(private store: Store<State>) {
+  }
+
+  ngOnInit(): void {
+    console.log("dispatching!");
+    this.store.dispatch(new PlanetsFetchAction());
+  }
+
+  public getCards(){
+
   }
 }
